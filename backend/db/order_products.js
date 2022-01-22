f788de7d2895b87d62367522b36b1bc1289cd76b
Maki Karijolich <maki.kuroi84@gmail.com>
2022-01-22 16:11:25 -0600

    * add stuff

--- backend/db/order_products.js
+++ backend/db/order_products.js
@@ -2,26 +2,52 @@ const client = require("./client");
 
 async function getProductByOrderId(id) {
   try {
+    const { rows: [productById] } = await client.query(`
+      SELECT *
+      FROM order_products
+      WHERE id=$1
+    `,[id])
+
+      return productById;
+
   } catch (error) {
     throw error;
   }
 }
 
-async function updateProductByOrderId() {
-  try {
-  } catch (error) {
-    throw error;
-  }
-}
+async function updateProductByOrderId(fields = {}) {
+  
+    const id = fields.id
 
+    const filteredFields = {}
+  
+    Object.keys(fields).forEach(key => {
+      if (key !== 'id') {
+          filteredFields[key] = fields[key];
+      }
+  });
 
+  const setString = Object.keys(filteredFields).map(
+      (key, index) => `"${key}"=$${index + 1}`
+    ).join(', ');
 
-async function updateProduct() {
-    try {
-    } catch (error) {
-      throw error;
+      if (setString.length === 0) {
+      return;
     }
-  }
+
+    const { rows: [productById]} = await client.query(`
+      UPDATE order_products
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *
+      `, Object.values(filteredFields));
+  
+      return productById;
+}
+
+
+
+
 
 
 module.exports = {

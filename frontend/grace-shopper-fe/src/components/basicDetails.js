650d07c452db609c94f204a0e09fad0057a0d842
Maki Karijolich <maki.kuroi84@gmail.com>
2022-02-07 22:02:53 -0600

    * fixed file names

--- frontend/grace-shopper-fe/src/components/basicDetails.js
+++ frontend/grace-shopper-fe/src/components/basicDetails.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+
 
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';

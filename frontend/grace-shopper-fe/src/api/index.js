b14e3d020123f073440e86fa7752aa4a4d949f8a
Maki Karijolich <maki.kuroi84@gmail.com>
2022-02-12 22:03:24 -0600

    update configs

--- frontend/grace-shopper-fe/src/api/index.js
+++ frontend/grace-shopper-fe/src/api/index.js
@@ -1,6 +1,6 @@
 import { loadTokenFromLocalStorage } from "../helpers/tokenHelpers";
 
-export const BASE_URL = "http://localhost:4000/api";
+export const BASE_URL = "https://grace-shopper-2022.herokuapp.com/api";
 
 const getHeaders = () => {
   const { token } = loadTokenFromLocalStorage();

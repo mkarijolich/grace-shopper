022a0639a973441bec145ce913e8d7d320836592
Maki Karijolich <maki.kuroi84@gmail.com>
2022-12-01 22:01:28 -0600

    * generalize db client

--- backend/db/client.js
+++ backend/db/client.js
@@ -1,6 +1,4 @@
 const { Client } = require('pg');
-const DB_NAME = 'graceshopper-dev'
-const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`;
-const client = new Client(DB_URL);
+const client = new Client();
 
 module.exports = client;
